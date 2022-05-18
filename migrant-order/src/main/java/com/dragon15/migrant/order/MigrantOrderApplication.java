9ccbe6e2957adc89ec9982c42acd69f411f05fa3
dragon15 <LIULONG1922615@outlook.com>
2022-05-18 21:45:00 +0800

    2022/5/18/21点44分

---
+++ migrant-order/src/main/java/com/dragon15/migrant/order/MigrantOrderApplication.java
@@ -0,0 +1,13 @@
+package com.dragon15.migrant.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MigrantOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MigrantOrderApplication.class, args);
+    }
+
+}
---
+++ migrant-order/src/test/java/com/dragon15/migrant/order/MigrantOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dragon15.migrant.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MigrantOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
